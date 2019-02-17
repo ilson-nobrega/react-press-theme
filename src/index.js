024f3b1e29537a73330989ce2e15d5fe32c4bf79
Ilson Nóbrega <ilson.nobrega@unyleuya.com.br>
2019-02-17 11:51:21 -0300

    React support added. Closes #2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './scss/style.scss';
+import React from 'react';
+import {render} from 'react-dom';
 
-console.log('It works');
+const App = () => {
+    return (
+        <div>
+            <h3>It works</h3>
+            <p>Our amazing application.</p>
+            <p>Yes, we can use React with Wordpress.</p>
+        </div>
+    );
+};
+
+render(<App/>, document.getElementById('app'));
